54fb66180fc5745447b7390caa77bc1ebf926f7e
Чукичев Станислав Дмитриевич <sosiska515@gmail.com>
2023-01-28 21:31:13 +0500

    add Lesson 1 Task 2

---
+++ Lesson1Task2/Program.cs
@@ -0,0 +1,20 @@
+﻿//Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+//a = 5; b = 7 -> max = 7
+//a = 2 b = 10 -> max = 10
+//a = -9 b = -3 -> max = -3
+
+int number1 = 3, number2 = 5;
+if (number1 > number2)
+{
+    Console.Write("max = ");
+    Console.WriteLine(number1);
+    Console.Write("min = ");
+    Console.WriteLine(number2);
+}
+else 
+{
+    Console.Write("max = ");
+    Console.WriteLine(number2);
+    Console.Write("min = ");
+    Console.WriteLine(number1);
+} 
