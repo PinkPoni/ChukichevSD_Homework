bf7c70a6ce380fe1b17a147794061d10e5cc567b
Чукичев Станислав Дмитриевич <sosiska515@gmail.com>
2023-02-02 20:43:45 +0500

    add Lesson 2 Task 10

--- Lesson1Task4/Program.cs
+++ Lesson1Task4/Program.cs
@@ -2,7 +2,14 @@
 //2, 3, 7 -> 7
 //44 5 78 -> 78
 //22 3 9 -> 22
+
 int number1 = 1, number2 = 6, number3 = 4;
+/*int max = number1;
+if (number2 > max) max = number2;
+if (number3 > max) max = number3;
+Console.Write("max = ");
+Console.WriteLine(max);
+*/
 if (number1 > number2)
 {
     if (number1 > number3)
---
+++ Lesson2Task10/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Введите трехзначное число: ");
+int count = Convert.ToInt32(Console.ReadLine());
+int result1 = count / 10;
+int result = result1%10;
+Console.WriteLine(result);
