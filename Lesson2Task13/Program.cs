9f6727c25268937f4781118a142c0ebfe2c38a28
Чукичев Станислав Дмитриевич <sosiska515@gmail.com>
2023-02-02 22:46:30 +0500

    add Lesson 2 Task 13

---
+++ Lesson2Task13/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите число: ");
+int count = Convert.ToInt32(Console.ReadLine());
+if (count < 100) 
+{
+    Console.WriteLine("Во введенном числе отсутствует третья цифра");
+}
+else 
+{
+while (count > 999)
+{
+    count = count / 10;
+}
+Console.WriteLine($"Третья цифра в числе - {count % 10}");
+}
