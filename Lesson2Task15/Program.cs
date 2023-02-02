ebc3a146a7ea074d67314ed54ccd67d14efac02e
Чукичев Станислав Дмитриевич <sosiska515@gmail.com>
2023-02-02 23:02:31 +0500

    add Lesson 2 Task 15

---
+++ Lesson2Task15/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите цифру, обозначающую день недели (от 1 до 7): ");
+int day = Convert.ToInt32(Console.ReadLine());
+if (day == 6 || day == 7) 
+{
+    Console.WriteLine("Выходной");
+}
+else 
+{
+    Console.WriteLine("Рабочий день");
+}
