78f8614c8fce39e4e53de2c8efce91126a76b07d
Чукичев Станислав Дмитриевич <sosiska515@gmail.com>
2023-02-07 23:40:29 +0500

    add Lesson 3 Task 23

---
+++ Lesson3Task23/Program.cs
@@ -0,0 +1,13 @@
+﻿//Задача 23
+//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+//3 -> 1, 8, 27
+//5 -> 1, 8, 27, 64, 125
+
+Console.WriteLine("Введите число: ");
+int count = Convert.ToInt32(Console.ReadLine());
+int num = 1;
+while (num <= count)
+{
+    Console.Write(num*num*num + " ");
+    num++;
+}
